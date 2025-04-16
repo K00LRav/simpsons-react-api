0bd7a5f36dc99e8a1f28f27d49cb12f8ab0dea75
K00LRav <k00lrav@gmail.com>
2025-04-16 01:49:40 -0400

    made changes to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,9 @@ import './Header.css';
 function Header() {
   return (
     <header className="header-container">
-      <a href="/" aria-label="Home - Quotes From The Simpsons">
-        Quotes From The Simpsons
+      <a href="/" aria-label="Home - Springfield Speaks" className="header-link">
+        <img src="/public/vite.png" alt="SpringfieldSpeaks Logo" className="header-logo" />
+        Springfield Speaks
       </a>
     </header>
   );
