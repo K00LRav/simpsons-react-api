bd471c27a170d7763dec93d48621f088f58644a6
K00LRav <k00lrav@gmail.com>
2025-04-16 02:02:11 -0400

    made changes to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header className="header-container">
       <a href="/" aria-label="Home - Springfield Speaks" className="header-link">
-        <img src="/public/vite.png" alt="SpringfieldSpeaks Logo" className="header-logo" />
+        <img src="/vite.png" alt="SpringfieldSpeaks Logo" className="header-logo" />
         Springfield Speaks
       </a>
     </header>
